2016a2b1b91e1efb02694849695b411c261cb8e2
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-10-03 17:03:43 +0200

    Visual grid
    Now grids has visual effect and are valid.

---
+++ Strategy/Assets/__Scripts/Actions/MoveAction.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAction : MonoBehaviour
+{
+    private Animator animator;
+    private Unit unit;
+
+    private float stoppingDistance = 0.1f;
+    private Vector3 targetPosition;
+
+    [SerializeField] int maxMoveDistance = 4;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        unit = GetComponent<Unit>();
+
+        targetPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            float moveSpeed = 4f;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
+            float rotationSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
+            animator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            animator.SetBool("IsWalking", false);
+        }
+    }
+
+    public void Move(GridPosition gridPosition)
+    {
+        this.targetPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
+    }
+
+    public bool IsValidActionGridPosition(GridPosition gridPosition)
+    {
+        List<GridPosition> validGridPositionList = GetValidActionGridPositionList();
+        return validGridPositionList.Contains(gridPosition);
+    }
+
+    public List<GridPosition> GetValidActionGridPositionList()
+    {
+        List<GridPosition> validGridPositionList = new List<GridPosition>();
+
+        GridPosition unitGridPosition = unit.GetGridPosition();
+
+        for (int x = -maxMoveDistance; x <= maxMoveDistance; x++)
+        {
+            for (int z = -maxMoveDistance; z < maxMoveDistance; z++)
+            {
+                GridPosition offsetGridPosition = new GridPosition(x, z);
+                GridPosition testGridPosition = unitGridPosition + offsetGridPosition;
+
+                if (!LevelGrid.Instance.IsValidGridPosition(testGridPosition)) continue;
+                if (unitGridPosition == testGridPosition) continue;
+                if (LevelGrid.Instance.HasAnyUnitOnGridPosition(testGridPosition)) continue;
+
+                validGridPositionList.Add(testGridPosition);
+            }
+        }
+
+        return validGridPositionList;
+    }
+}
