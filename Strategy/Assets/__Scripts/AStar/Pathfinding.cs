1956d3f2430c990d656c9657d12ec3608f264b48
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-10-10 19:35:27 +0200

    Obstacles
    Added obstacles into pathfinding script.

--- Strategy/Assets/__Scripts/AStar/Pathfinding.cs
+++ Strategy/Assets/__Scripts/AStar/Pathfinding.cs
@@ -11,6 +11,7 @@ public class Pathfinding : MonoBehaviour
     private const int MOVE_DIAGONAL_COST = 14;
 
     [SerializeField] private Transform gridDebugPrefab;
+    [SerializeField] private LayerMask obstacleLayerMask;
 
     private int width;
     private int height;
@@ -27,11 +28,34 @@ public class Pathfinding : MonoBehaviour
         }
 
         Instance = this;
+    }
 
-        gridSystem = new GridSystem<PathNode>(10, 10, 2f,
+    public void SetUp(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridSystem = new GridSystem<PathNode>(width, height, cellSize,
             (GridSystem<PathNode> g, GridPosition gridPosition) => new PathNode(gridPosition));
 
         gridSystem.CreateDebugObject(gridDebugPrefab);
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                GridPosition gridPosition = new GridPosition(x, z);
+                Vector3 worldPosition = LevelGrid.Instance.GetWorldPosition(gridPosition);
+
+                float raycastOffsetDistance = 5f;
+                if (Physics.Raycast(worldPosition + Vector3.down * raycastOffsetDistance, Vector3.up, raycastOffsetDistance * 2,
+                    obstacleLayerMask))
+                {
+                    GetNode(x, z).SetIsWalkable(false);
+                }
+            }
+        }
     }
 
     public List<GridPosition> FindPath(GridPosition startGridPosition, GridPosition endGridPosition)
@@ -77,6 +101,12 @@ public class Pathfinding : MonoBehaviour
             {
                 if (closeList.Contains(neighbourNode))
                 {
+                    continue;
+                }
+
+                if (!neighbourNode.IsWalkable())
+                {
+                    closeList.Add(neighbourNode);
                     continue;
                 }
 
--- Strategy/Assets/__Scripts/GridSystem/LevelGrid.cs
+++ Strategy/Assets/__Scripts/GridSystem/LevelGrid.cs
@@ -8,6 +8,9 @@ public class LevelGrid : MonoBehaviour
     public static LevelGrid Instance { get; private set; }
 
     [SerializeField] Transform gridDebugPrefab;
+    [SerializeField] private int width;
+    [SerializeField] private int height;
+    [SerializeField] private float cellSize;
 
     private GridSystem<GridObject> gridSystem;
 
@@ -24,10 +27,15 @@ public class LevelGrid : MonoBehaviour
 
         Instance = this;
 
-        gridSystem = new GridSystem<GridObject>(10, 10, 2f,
+        gridSystem = new GridSystem<GridObject>(width, height, cellSize,
             (GridSystem<GridObject> g, GridPosition gridPosition) => new GridObject(g, gridPosition));
 
         //gridSystem.CreateDebugObject(gridDebugPrefab);
+    }
+
+    private void Start()
+    {
+        Pathfinding.Instance.SetUp(width, height, cellSize);
     }
 
     public void AddUnitAtGridPosition(GridPosition gridPosition, Unit unit)
