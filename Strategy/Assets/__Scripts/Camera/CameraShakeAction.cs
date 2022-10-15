e0e68012555c525d210655fa37aebcbcc9504c26
Sebastian Hara <48924353+SeborExe@users.noreply.github.com>
2022-10-15 14:57:56 +0200

    Grenade
    Grenade action has been created.

--- Strategy/Assets/__Scripts/Camera/CameraShakeAction.cs
+++ Strategy/Assets/__Scripts/Camera/CameraShakeAction.cs
@@ -7,6 +7,12 @@ public class CameraShakeAction : MonoBehaviour
     private void Start()
     {
         ShootAction.OnAnyShoot += ShootAction_OnAnyShoot;
+        GrenadeProjectile.OnAnyGrenadeExploded += GrenadeProjectile_OnAnyGrenadeExploded;
+    }
+
+    private void GrenadeProjectile_OnAnyGrenadeExploded(object sender, System.EventArgs e)
+    {
+        ScreenShake.Instance.Shake(10f);
     }
 
     private void ShootAction_OnAnyShoot(object sender, ShootAction.OnShootEventArgs e)
